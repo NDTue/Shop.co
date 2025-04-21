1ebeed6645411c57a5bb4ccfcd8b651877b9fee7
NDTue <ductue2003@gmail.com>
2025-04-21 08:38:35 +0700

    [ProductList] Modified Layout

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ProductCard from './ProductCard';
 import products from '../../data/products';
 
-function ProductList(props) {
+function ProductList({className = 'md:grid-cols-4'}) {
     return (
         
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-x-5">
+        <div className={`grid grid-cols-2 ${className} gap-x-5 gap-y-9 justify-items-center md:justify-items-start`}>
             {products
                 .map(product => (
                     <ProductCard key={product.id} product={product} />
