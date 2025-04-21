4e326674f121865c15b723465e8204a393098551
NDTue <ductue2003@gmail.com>
2025-04-21 12:08:01 +0700

    [ProductList] add prop

--- src/components/product/ProductList.jsx
+++ src/components/product/ProductList.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProductCard from './ProductCard';
 import products from '../../data/products';
 
-function ProductList({className = 'md:grid-cols-4'}) {
+function ProductList({className = 'md:grid-cols-4', products}) {
     return (
         
         <div className={`grid grid-cols-2 ${className} gap-x-5 gap-y-9 justify-items-center md:justify-items-start`}>
