69ab4f4741202203834a0dc640de84b49c47f5a0
NDTue <ductue2003@gmail.com>
2025-04-21 08:40:45 +0700

    [ProductCard] Generate Slug

--- src/components/product/ProductCard.jsx
+++ src/components/product/ProductCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import products from '../../data/products';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 ProductCard.propTypes = {
     product: PropTypes.shape({
@@ -13,11 +14,23 @@ ProductCard.propTypes = {
 }
 
 function ProductCard({ product }) {
-    return (
-        <div className=''>
-            <img className='rounded-xl w-52 h-52 md:w-64 md:h-64'
-                src={product.image} alt={product.name} />
+    // Hàm tạo Slug
+    const generateSlug = (str) => (
+        str.toLowerCase()  // Chuyển thành chữ thường
+            .replace(/[^a-z0-9]+/g, '-')  // Thay khoảng trắng và ký tự đặc biệt bằng dấu '-'
+            .replace(/^-|-$/g, '')  // Xóa dấu '-' ở đầu và cuối chuỗi
+    )
+    const slug = generateSlug(product.name)
 
+    return (
+        <Link to={`/product/${slug}`} className='w-full'>
+            <div className="aspect-square overflow-hidden rounded-3xl"> {/* Thêm container với tỷ lệ 1:1 */}
+                <img
+                    className="w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out"
+                    src={product.image}
+                    alt={product.name}
+                />
+            </div>
             <h3 className='text-sm pt-2 font-bold'>{product.name}</h3>
 
             <div className='flex text-sm tracking-widest'>
@@ -34,8 +47,31 @@ function ProductCard({ product }) {
             </div>
             <p className='font-bold text-lg'>${product.price}</p>
 
-        </div>
+        </Link>
     );
 }
 
 export default ProductCard;
+// <Link to='/product' className='w-full'>
+//     <div className="overflow-hidden rounded-3xl">
+//         <img className="hover:scale-105 transition-transform duration-300 ease-in-out w-full h-52 md:h-64 "
+//             src={product.image} alt={product.name} />
+//     </div>
+
+//     <h3 className='text-sm pt-2 font-bold'>{product.name}</h3>
+
+//     <div className='flex text-sm tracking-widest'>
+//         <div>
+//             <i className="text-[#ffc633] fa-sharp fa-solid fa-star"></i>
+//             <i className="text-[#ffc633] fa-sharp fa-solid fa-star"></i>
+//             <i className="text-[#ffc633] fa-sharp fa-solid fa-star"></i>
+//             <i className="text-[#ffc633] fa-sharp fa-solid fa-star"></i>
+//             <i className="text-[#ffc633] fa-sharp fa-solid fa-star-half"></i>
+//         </div>
+//         <div className="">
+//             4.5/5
+//         </div>
+//     </div>
+//     <p className='font-bold text-lg'>${product.price}</p>
+
+// </Link>
