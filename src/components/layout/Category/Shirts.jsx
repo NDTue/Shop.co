4466954dc2963166ebf24f8529c3adc79cfd9b00
NDTue <ductue2003@gmail.com>
2025-04-22 17:33:43 +0700

    [Categories - Item Files] Modified Homepage Path

--- src/components/layout/Category/Shirts.jsx
+++ src/components/layout/Category/Shirts.jsx
@@ -36,14 +36,14 @@ function Shirts(props) {
                 <nav className="pt-24 md:pt-36 mb-4 md:mb-5">
                     <div className='flex flex-row justify-between items-center md:gap-10 max-w-96 md:max-w-[78rem] my-0 mx-auto xl:mx-auto md:ml-16 md:mr-16'>
                         <ol className="flex items-center whitespace-nowrap">
-                            <li className="inline-flex items-center">
+                            <Link to='/' className="inline-flex items-center">
                                 <a className="flex items-center text-sm text-gray-500 hover:text-black focus:outline-hidden focus:text-blue-600" href="#">
                                     Home
                                 </a>
                                 <svg className="shrink-0 mx-2 size-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                                     <path d="m9 18 6-6-6-6"></path>
                                 </svg>
-                            </li>
+                            </Link>
                             <li className="inline-flex items-center">
                                 <Link to='/category' className="flex items-center text-sm text-gray-500 hover:text-black focus:outline-hidden focus:text-blue-600" href="#">
                                     Category
