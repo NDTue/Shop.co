2425d536350873535756bf0580cdbf0cdfe8fdd0
NDTue <ductue2003@gmail.com>
2025-04-23 10:15:00 +0700

    [Cart] backup

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import Header from '../components/common/Header';
+import Footer from '../components/common/Footer';
+import { Link } from 'react-router-dom';
 
 function Cart(props) {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header />
+            <main>
+                {/* Bread Crumb */}
+                <nav className="pt-24 md:pt-36 mb-4 md:mb-5">
+                    <div className='flex flex-row justify-between items-center md:gap-10 max-w-96 md:max-w-[78rem] my-0 mx-auto xl:mx-auto md:ml-16 md:mr-16'>
+                        <ol className="flex items-center whitespace-nowrap">
+                            <li className="inline-flex items-center">
+                                <Link to='/' className="flex items-center text-sm text-gray-500 hover:text-black focus:outline-hidden focus:text-blue-600" href="#">
+                                    Home
+                                </Link>
+                                <svg className="shrink-0 mx-2 size-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                    <path d="m9 18 6-6-6-6"></path>
+                                </svg>
+                            </li>
+                            <li className="inline-flex items-center text-sm font-semibold text-gray-800 truncate" aria-current="page">
+                                Cart
+                            </li>
+                        </ol>
+                    </div>
+                </nav>
+
+                <section className=''>
+                    <div className='max-w-96 md:max-w-[78rem] my-0 mx-auto'>
+                        <h1 className='font-extrabold text-3xl'>YOUR CART</h1>
+                        <div className="grid grid-cols-1 md:grid-cols-2">
+                            {/* Product Items */}
+                            <div className="">
+                                
+                            </div>
+                            {/* Order Summary */}
+                            <div className="">
+
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <div className="h-[500px]"></div>
+            <Footer />
+        </>
     );
 }
 
