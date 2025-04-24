4f5a2f1b25aef6d083f34760a7d914ed064ae694
NDTue <ductue2003@gmail.com>
2025-04-24 18:04:01 +0700

    [main] Add Scroll To Top

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { CartProvider } from './components/layout/Cart/CartContext.jsx'
+import ScrollToTop from './components/common/ScrollTop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename='/Shop.co/'>
       <CartProvider>
+        <ScrollToTop/>
         <App />
       </CartProvider>
     </BrowserRouter>
