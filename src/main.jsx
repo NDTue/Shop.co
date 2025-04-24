b081b1cf87f871c746c642691c5072f992ca532f
NDTue <ductue2003@gmail.com>
2025-04-24 16:35:56 +0700

    [main] Updated config

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import { CartProvider } from './components/layout/Cart/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+  //   <BrowserRouter>
+  //     <CartProvider>
+  //       <App />
+  //     </CartProvider>
+  //   </BrowserRouter>
+  // </StrictMode>,
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CartProvider>
         <App />
       </CartProvider>
-    </BrowserRouter>
-
+    </HashRouter>
   </StrictMode>,
 )
