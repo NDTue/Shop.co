c9913dbb58b613cbd9be9e4114a10f37178ebe86
NDTue <ductue2003@gmail.com>
2025-04-24 17:29:23 +0700

    [ViteConfig] Updated assets

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Shop.co/',
+  build: {
+    assetsDir: 'assets', // Thư mục chứa assets
+  }
 })
