62426574479553c42e63820bcfbefccefbb9c1f1
NDTue <ductue2003@gmail.com>
2025-04-28 08:43:22 +0700

    [ViteConfig] Config to deploy on Github Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+/*
+  Nếu deploy trên GitHub Pages, base = '/Shop.co/'
+  Nếu deploy trên Vercel, base = '/'
+  Kiểm tra process.env.VERCEL để nhận diện Vercel.
+*/
+
+const isGitHubPages = process.env.NODE_ENV === "production" && process.env.VERCEL !== "1";
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Shop.co/',
+  base: isGitHubPages ? "/Shop.co/" : "/",
   build: {
     assetsDir: 'assets', // Thư mục chứa assets
   }
